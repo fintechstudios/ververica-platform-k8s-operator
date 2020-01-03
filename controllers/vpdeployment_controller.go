83ad1d8d5119e96701351b6aa3f84f3cbe8cb235
austin ce <austin.cawley@gmail.com>
2020-01-02 19:15:33 -0500

    feat(savepoint): attach deployment

--- controllers/vpdeployment_controller.go
+++ controllers/vpdeployment_controller.go
@@ -247,6 +247,26 @@ func (r *VpDeploymentReconciler) addStatusPollerForResource(req ctrl.Request, vp
 	r.pollerManager.AddPoller("status", req.String(), poller)
 }
 
+
+func (r *VpDeploymentReconciler) addSavepointPollerForResource(req ctrl.Request, vpDeployment *v1beta1.VpDeployment) {
+	nsName := utils.GetNamespaceOrDefault(vpDeployment.Spec.Metadata.Namespace)
+	vpID := annotations.Get(vpDeployment.Annotations, annotations.ID)
+	poller := polling.NewPoller(r.getStatusPollerFunc(req, nsName, vpID), statusPollingInterval)
+
+	r.pollerManager.AddPoller("status", req.String(), poller)
+}
+
+func (r *VpDeploymentReconciler) getSavepointPollerFunc(req ctrl.Request, namespace, id string) polling.PollerFunc {
+	log := r.getLogger(req).WithValues("poller", "savepoint")
+	return func() interface{} {
+		log.Info("Polling")
+		// get a list of savepoints and create ones that do not yet exist in k8s
+
+
+		return nil
+	}
+}
+
 // updateResource takes a k8s resource and a VP resource and syncs them in k8s - does a full update
 func (r *VpDeploymentReconciler) updateResource(resource *v1beta1.VpDeployment, deployment *appmanagerapi.Deployment) error {
 	ctx := context.Background()
--- controllers/vpsavepoint_controller.go
+++ controllers/vpsavepoint_controller.go
@@ -29,6 +29,7 @@ import (
 	"github.com/fintechstudios/ververica-platform-k8s-operator/controllers/utils"
 	vvperrors "github.com/fintechstudios/ververica-platform-k8s-operator/controllers/vvp-errors"
 	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -88,7 +89,6 @@ func (r *VpSavepointReconciler) getStatusPollerFunc(req ctrl.Request, namespace,
 func (r *VpSavepointReconciler) addStatusPollerForResource(req ctrl.Request, vpSavepoint *v1beta1.VpSavepoint) {
 	nsName := utils.GetNamespaceOrDefault(vpSavepoint.Spec.Metadata.Namespace)
 	vpID := annotations.Get(vpSavepoint.Annotations, annotations.ID)
-
 	// On each polling callback, push the update through the k8s client
 	poller := polling.NewPoller(r.getStatusPollerFunc(req, nsName, vpID), time.Second*5)
 	r.pollerManager.AddPoller("status", req.String(), poller)
@@ -139,6 +139,7 @@ func (r *VpSavepointReconciler) handleCreate(req ctrl.Request, vpSavepoint v1bet
 		return ctrl.Result{Requeue: false}, nil
 	}
 	depID := vpSavepoint.Spec.Metadata.DeploymentID
+	var depNamespacedName types.NamespacedName
 	if depID == "" {
 		// no deployment id has been explicitly set
 		// try to find one
@@ -155,6 +156,10 @@ func (r *VpSavepointReconciler) handleCreate(req ctrl.Request, vpSavepoint v1bet
 		}
 
 		depID = deployment.Metadata.Id
+		depNamespacedName = types.NamespacedName{
+			Namespace: vpSavepoint.Namespace,
+			Name:      depName,
+		}
 	}
 
 	createdSavepoint, res, err := r.AppManagerAPIClient.SavepointsApi.CreateSavepoint(ctx, nsName, appmanagerapi.Savepoint{
@@ -175,6 +180,20 @@ func (r *VpSavepointReconciler) handleCreate(req ctrl.Request, vpSavepoint v1bet
 	if err != nil {
 		log.Info("Error creating VP Savepoint")
 		return ctrl.Result{}, err
+	}
+
+	// If there exists a VpDeployment, attach it as the owner of this savepoint
+	var vpDeployment v1beta1.VpDeployment
+
+	if err := r.Get(context.Background(), depNamespacedName, &vpDeployment); err != nil {
+		if !utils.IsNotFoundError(err) {
+			return ctrl.Result{}, err
+		}
+		log.WithValues("VpDeployment name", depNamespacedName.String()).
+				Info("No VpDeployment")
+	} else {
+		// we've got a deployment, set it!
+		vpSavepoint.OwnerReferences = append(vpSavepoint.OwnerReferences, &vpDeployment)
 	}
 
 	// Now update the k8s resource and status as well
