9b72a696deb625f897e9f9672e8fef7820c16110
austin ce <austin.cawley@gmail.com>
2020-07-17 18:46:04 -0400

    revert test

--- api/v1beta2/vpdeploymenttarget_conversion_test.go
+++ api/v1beta2/vpdeploymenttarget_conversion_test.go
@@ -37,11 +37,7 @@ var _ = Describe("VpDeploymentTarget conversion", func() {
 			Value: pointer.StringPtr("gazelle"),
 		},
 	}
-	jsonPatchStr := `[{
-		"op": "add",
-		"path": "/hello",
-		"value": "gazelle"
-	}]`
+	jsonPatchStr := "[{\"op\":\"add\",\"path\":\"/hello\",\"value\":\"gazelle\"}]"
 
 	It("should convert to the hub", func() {
 		// [{"op":"add","path":"/hello","value":"gazelle"}]
