3df085fe6a1eb6270eb5776d2d90e7bb02ae2bc3
austin ce <austin.cawley@gmail.com>
2020-08-07 15:41:03 -0400

    feat(pkg): add fixed clock for testing

--- pkg/scheduling/clock.go
+++ pkg/scheduling/clock.go
@@ -6,8 +6,26 @@ type realClock struct{}
 
 func (*realClock) Now() time.Time { return time.Now() }
 
+// NewClock should always be used to create a NewClock for runtime code
 func NewClock() Clock {
 	return &realClock{}
+}
+
+type fixedClock struct {
+	fixedTime *time.Time
+}
+
+func (f *fixedClock) Now() time.Time {
+	return *f.fixedTime
+}
+
+// NewFixedClock creates a Clock that always returns the same time pointer.
+// This allows tests to manipulate the current time.
+func NewFixedClock(fixedTime *time.Time) Clock {
+	if fixedTime == nil {
+		panic("fixed time cannot be nil")
+	}
+	return &fixedClock{fixedTime}
 }
 
 // Clock knows how to get the current time.
